9778725c0f995fc71540976fbfcdefca77e1c66d
Sergio0799 <sergiomontoya0723@gmail.com>
2022-10-20 12:59:38 -0500

    Cleaned & commented the code

--- SweetSaltyChallenge/Program.cs
+++ SweetSaltyChallenge/Program.cs
@@ -1,57 +1,49 @@
-﻿/*
-Get the start and stop numbers (within the 1000 range) from the user. 
+﻿int startNum, stopNum, perLine;
 
-You must re-prompt if the difference between the numbers is more than 1000. 
-
-If the numbers are given in the wrong order, re-prompt. (ex. first number 1000, second number 100.)
-
-Get the quantity of numbers you will print on each line from the user. 
-
-You must re-prompt if the quantity chosen by the user is greater than 30 or less than 5 
-
-Print the numbers in groups of what the user says per line with one space separating each number. 
-
-If the number is a multiple of three, print “Sweet” (instead of the number) to the console.     
-
-If the number is a multiple of five, print “Salty” (instead of the number) to the console.     
-
-For numbers which are multiples of both three and five, print “Sweet’nSalty” to the console (instead of the number).     
-
-After the numbers have all been printed to the console, print to the console on 3 separate lines, 
-
-how many "Sweet",  
-
-how many "Salty", and  
-
-how many "Sweet’nSalty" numbers there were.  
-
-'Sweet', 'Salty', and 'Sweet'nSalty' are three separate groups, so "Sweet’nSalty" does not increment "Sweet" or "Salty" (and vice versa).    
-
-Include verbose commentary in the source code to tell me what each few lines are doing. 
-
-Make sure to merge this branch to your main branch, by 5pm today. 
-*/
-
-int startNum, stopNum, diff, perLine;
-
+// Loop to validate startNum & stopNum inputs
 do {
+    // Start & Stop are assigned to valid ints input by user
     startNum = NumInput("Start Number: ");
     stopNum = NumInput("Stop Number: ");
-    diff = stopNum - startNum;
-} while (stopNum - startNum < 0 || stopNum - startNum > 1000);
 
+    // Print warnings when Start & Stop numbers don't meet constraints
+    if (stopNum - startNum < 0)
+        Console.WriteLine("\n***Start # must be smaller than Stop #***");
+    else if (stopNum - startNum > 1000)
+        Console.WriteLine("\n***Numbers cannot be more than 1000 apart***");
+    // Only breaks out of loop if start & stop meet constraints
+    else    break;
+
+    Console.WriteLine("\n---Please Re-Input Numbers---");
+} while (true);
+
+// Loop to validate perLine input
 do {
+    // PerLine is assigned to valid int input by user
     perLine = NumInput("How many numbers should print per line?");
-} while (perLine < 5 || perLine > 30);
 
+    // Print warnings when number doesn't meet constraints
+    if (perLine < 5)
+        Console.WriteLine("\n***Must print at least 5 per line***");
+    else if (perLine > 30)
+        Console.WriteLine("\n***Only up to 30 per line***");
+    // Only breaks out of loop if perLine meets constraints
+    else    break;
+} while (true);
+
+// Instantiate counters to 0 before loop
 int sweet = 0, salty = 0, sweetSalty = 0 , lineIndex = 0;
 
+// Iterates through each number from startNum to stopNum
 for (; startNum <= stopNum; startNum++) {
+    // Establishes new line on console once the desired amount of numbers are on current one
     if (lineIndex == perLine) {
         Console.Write("\n");
         lineIndex = 0;
     }
 
+    // Print number OR phrase & increment counter for each number that meets pre-determined constraints:
+    // Sweet when multiple of 3, Salty when multiple of 5, Sweet'nSalty when both
     if (startNum % 3 == 0 && startNum % 5 == 0) {
         Console.Write("Sweet'nSalty ");
         sweetSalty++;
@@ -68,28 +60,28 @@ for (; startNum <= stopNum; startNum++) {
         Console.Write(startNum + " ");
     }
 
+    // Incement index of current line for each number's output
     lineIndex++;
 }
 
-Console.WriteLine("\n# of Sweet: " + sweet);
-Console.WriteLine("# of Salty: " + salty);
-Console.WriteLine("# of Sweet'nSalty: " + sweetSalty);
+// Print the total amount of Sweets, Saltys, & Sweet'nSaltys seperately 
+Console.WriteLine("\nAmount of Sweet: " + sweet);
+Console.WriteLine("Amount of Salty: " + salty);
+Console.WriteLine("Amount of Sweet'nSalty: " + sweetSalty);
 
 
-string RealInput(string msg) {
-    string input;
-    do {
-        if (msg != "")  Console.WriteLine(msg);
-        input = Console.ReadLine()!;
-    } while (String.IsNullOrWhiteSpace(input));
-    return input;
-}
-
+// Function to validate that user inputs an int in response to a prompt(msg)
 int NumInput(string msg) {
-    int number;
-    bool isNumber;
     do {
-        isNumber = int.TryParse(RealInput(msg), out number);
-    } while (!isNumber);
-    return number;
+        // Ask user for input through console
+        Console.WriteLine(msg);
+        string input = Console.ReadLine()!;
+
+        // Only break loop if user's input can be parsed to an int
+        if (!String.IsNullOrWhiteSpace(input))
+            if (int.TryParse(input, out int number)) return number;
+
+        // Print warning and loop if input is not a valid int
+        Console.WriteLine("***Invalid Input*** (Number Only)\n");
+    } while (true);
 }
